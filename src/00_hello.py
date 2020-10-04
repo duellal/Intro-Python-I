2f50808cea86ee2088bb377f7cfaf82df107afe0
User <user@Alexs-MacBook-Air.local>
2020-10-04 18:37:36 -0400

    Notes

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,18 @@
 # Print "Hello, world!" to your terminal
+#print("Hello, world!")
+
+greeting = "Hello, world!"
+number1 = 20
+floatNum = 19.25
+#print(greeting, number1, floatNum)
+
+x = 200
+y = 200
+
+if x < y:
+    print('x is less than y')
+elif x == y:
+    print('x is equal to y')
+else:
+    print('x is greater than y')
+
